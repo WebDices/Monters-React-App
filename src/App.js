ad7a87ceefbcfae25bffa4df4e6ef71a546d4739
Alexandru Buruiana <alexandru.buruiana8@gmail.com>
2020-04-03 17:59:42 +0300

    Props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component }  from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component'
 import './App.css';
+
+
 
 class App extends Component{
   constructor(){
@@ -24,10 +26,19 @@ class App extends Component{
       ]
     }
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then( response => response.json())
+    .then(users => this.setState( {monsters: users }));
+  }
+
   render(){
       return (
         <div className="App">
-     
+        <CardList name="Alex">
+          <h1>Alex</h1>
+        </CardList>
         { this.state.monsters.map(monster =>(
           <h1 key={monster.id}> {monster.name} </h1>
         ))}
