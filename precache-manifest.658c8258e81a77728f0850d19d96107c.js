0084361204be4248a34946d153395fa35b4a6939
Alexandru Buruiana <alexandru.buruiana8@gmail.com>
2020-04-04 14:50:07 +0300

    Updates

---
+++ precache-manifest.658c8258e81a77728f0850d19d96107c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "547c754a8b6b645fea3174da27830749",
+    "url": "/Monters-React-App/index.html"
+  },
+  {
+    "revision": "94d435380d8b1a2ade0f",
+    "url": "/Monters-React-App/static/css/main.c94ed646.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/Monters-React-App/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monters-React-App/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94d435380d8b1a2ade0f",
+    "url": "/Monters-React-App/static/js/main.c89ee675.chunk.js"
+  },
+  {
+    "revision": "80e1a138c0498e7c0cc2",
+    "url": "/Monters-React-App/static/js/runtime-main.a0b1a791.js"
+  }
+]);
